c2fbff680f794a134bb8e06981ecd042de41dfbb
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2020-12-02 17:07:10 +0000

    Update zeroapply-scalaz to 0.2.3

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object build extends Build {
     libraryDependencies ++= (
       ("com.chuusai" %% "shapeless" % "2.2.5") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.1") ::
-      ("com.github.xuwei-k" %% "zeroapply-scalaz" % "0.2.0" % "provided") ::
+      ("com.github.xuwei-k" %% "zeroapply-scalaz" % "0.2.3" % "provided") ::
       ("com.github.alexarchambault" %% "scalacheck-shapeless_1.12" % "0.3.1" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.2.0" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
